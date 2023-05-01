5b9499e62d08bc05bd0643ade64a062cf2c9bfde
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-01 13:20:00 -0500

    Finished witht he POST section. very free-form and straight forward.

--- my-course-code/project-1/books.py
+++ my-course-code/project-1/books.py
@@ -1,5 +1,5 @@
 # Import the fast API Package for obvious reasons. 
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 
 # import our dummy data.
@@ -53,3 +53,10 @@ async def read_author_category_by_query(book_author: str, category: str):
         if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
+
+
+
+# Our first POST Function
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
