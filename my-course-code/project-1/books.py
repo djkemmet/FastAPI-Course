5a43e7198d589de51a2efe9b0ddecffb6dc1a44f
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-01 13:45:12 -0500

    done with the delete explainations.

--- my-course-code/project-1/books.py
+++ my-course-code/project-1/books.py
@@ -68,3 +68,10 @@ async def update_book(updated_book=Body()):
         if BOOKS[i].get('title') == updated_book.get('title'):
             BOOKS[i] == update_book
     
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break # Stop searching, we've done the deed.
+    
