e9cc2dcd5acbeaf7f4fc6cbb0b1692440118bb2f
root <root@pop-os.localdomain>
2023-05-03 11:40:04 -0500

    Starting code up to section 6 video 69

--- my-course-code/Project2/books.py
+++ my-course-code/Project2/books.py
@@ -1,6 +1,8 @@
 
 # Import the FastAPI Framework
 from fastapi import FastAPI, Body 
+from pydantic import BaseModel, Field
+from typing import Optional
 
 # Create an instance of a FastAPI
 app = FastAPI()
@@ -25,6 +27,28 @@ class Book():
         self.description = description
         self.rating = rating
 
+#
+# Model Request Validator
+#
+class BookRequest(BaseModel):
+
+    # Class Properties
+    id: Optional[int]
+    title: str =  Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str =  Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    class Config:
+        schema_extra = {
+            'example': {
+                'Title': " The name of the book",
+                'Author': 'The person who wrote the book',
+                'Description': 'A breif synopsis of the book',
+                'Rating': 'A Numerical valude from 1 to 6 on the quality or experience of the book'
+            }
+        }
+
 
 
 # Create a data structure to hold the books in our API
@@ -43,5 +67,22 @@ async def read_all_books():
     return BOOKS
 
 @app.post("/create-book")
-async def create_book(book_request= Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    
+    # Create a new book object for the Keyword Arguments (** means kwargs)
+    # we're creating by unpacking the body as a dictionary. Basically,
+    # the request body is a dictionary that supplies the keywords to be 
+    # unpacked by the **
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:           
+         book.id = 1
+
+    # This should be the actuall object, not the refernce
+    # to the class.
+    return book
---
+++ my-course-code/Project2/main.py
@@ -0,0 +1,10 @@
+from books import app
+from fastapi import FastAPI
+import uvicorn
+
+app = app()
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8082)
