3e3ec1c6531a33c253fedd1ac2720e51d5eca0c9
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-04 11:44:18 -0500

    Starting with paramater validation

--- my-course-code/Project2/books.py
+++ my-course-code/Project2/books.py
@@ -18,14 +18,16 @@ class Book():
     author: str
     description: str
     rating: int
+    published_date: int
 
     # Class Constructor
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 #
 # Model Request Validator
@@ -38,14 +40,16 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str =  Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1600)
 
     class Config:
         schema_extra = {
             'example': {
-                'Title': " The name of the book",
-                'Author': 'The person who wrote the book',
-                'Description': 'A breif synopsis of the book',
-                'Rating': 'A Numerical valude from 1 to 6 on the quality or experience of the book'
+                'id': 'an optional parameter to identify the book',
+                'title': " The name of the book",
+                'author': 'The person who wrote the book',
+                'description': 'A breif synopsis of the book',
+                'rating': 'A Numerical valude from 1 to 6 on the quality or experience of the book'
             }
         }
 
@@ -53,19 +57,19 @@ class BookRequest(BaseModel):
 
 # Create a data structure to hold the books in our API
 BOOKS = [
-   Book(id=1, title="My Neat Book", author="Rob Tobulox", description="This is a description", rating=1),
-   Book(id=2, title="My Cool Book", author="Jurry Bobulox", description="This is a description", rating=5),
-   Book(id=3, title="My Keen Book", author="Felicity Glorgox", description="This is a description", rating=4),
-   Book(id=4, title="My Nifty Book", author="Michael Fremulon", description="This is a description", rating=4),
-   Book(id=5, title="My Radical Book", author="Prassad Gorbechav", description="This is a description", rating=5),
-   Book(id=6, title="My Awesome Book", author="Justice Northwind", description="This is a description", rating=3),
-   Book(id=7, title="My Mighty Book", author="Constantine Frindle", description="This is a description", rating=2),
-   Book(id=8, title="My Boring Book", author="Bobulox Arzronand", description="This is a description", rating=5)
-
+   Book(id=1, title="My Neat Book", author="Rob Tobulox", description="This is a description", rating=1, published_date=2000),
+   Book(id=2, title="My Cool Book", author="Jurry Bobulox", description="This is a description", rating=5, published_date=2000),
+   Book(id=3, title="My Keen Book", author="Felicity Glorgox", description="This is a description", rating=4, published_date=2000),
+   Book(id=4, title="My Nifty Book", author="Michael Fremulon", description="This is a description", rating=4, published_date=2000),
+   Book(id=5, title="My Radical Book", author="Prassad Gorbechav", description="This is a description", rating=5, published_date=2000),
+   Book(id=6, title="My Awesome Book", author="Justice Northwind", description="This is a description", rating=3, published_date=2000),
+   Book(id=7, title="My Mighty Book", author="Constantine Frindle", description="This is a description", rating=2, published_date=2000),
+   Book(id=8, title="My Boring Book", author="Bobulox Arzronand", description="This is a description", rating=5, published_date=2000)
 ]
 
-
-
+#
+# GETs
+#
 @app.get("/books")
 async def read_all_books():
     return BOOKS
@@ -84,7 +88,17 @@ async def get_book_by_rating(book_rating: int):
             books_to_return.append(book)
     return books_to_return
 
+@app.get("/books/in-year/{year}")
+async def get_book_by_year(year: int):
+    books_to_return = []
+    for book_number in range(len(BOOKS)):
+        if BOOKS[book_number].published_date == year:
+            books_to_return.add(BOOKS[book_number])
+    return books_to_return
 
+#
+# POSTs
+#
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     
@@ -95,8 +109,33 @@ async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
 
+#
+# PUTs
+#
+@app.put("/books/update-book") 
+# This function needs to unpack the data from the body
+# into a BookRequest validator
+async def update_book(book: BookRequest):
+    
+    # Iterate through our books
+    for book_number in range(len(BOOKS)):
 
+        # If the book we're currently looking at shares
+        # the ID of the updated book data we sent through
+        # the API
+        if BOOKS[book_number].id == book.id:
+            
+            # Replace the data at that index, effectively 
+            # updating the book.
+            BOOKS[book_number] = book
 
+@app.delete("/delete-book/{book_id}")
+async def delete_book(book_id: int):
+    print(f"Book ID is {book_id}")
+    for book_number in range(len(BOOKS)):
+        print()
+        if BOOKS[book_number].id == book_id:
+            BOOKS.pop(book_number)
 
 def find_book_id(book: Book):
     if len(BOOKS) > 0:
