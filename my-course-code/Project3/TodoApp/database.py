d43e9e507a9b6cb02dfacbc4d2761e908e09f3ce
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-05 12:07:57 -0500

    Completed up to video 84

---
+++ my-course-code/Project3/TodoApp/database.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Body, Query, HTTPException
+
+# Serves as the foundation for our application: https://docs.sqlalchemy.org/en/20/core/engines.html
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Ideally, a connection string as a variable.
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+# Create an instance of a connection to the database at our database URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# a Session for connecting to and interacting with our database.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Allows us to create database tables.
+Base = declarative_base()
---
+++ my-course-code/Project3/TodoApp/models.py
@@ -0,0 +1,18 @@
+# From SQLAlchemy import
+from sqlalchemy import Column, Integer, String, Boolean
+
+# Use the Base (datasource) we defined earlier
+from .database import Base
+
+
+class Todos(Base):
+    '''This class is responsible for defining the table that will store TODO Items.'''
+
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+
