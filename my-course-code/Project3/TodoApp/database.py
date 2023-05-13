b0f719d8d1dc30201983b61594fe829b363581a6
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-13 18:12:32 -0500

    Added the required Project 3.5 Starter Files

--- my-course-code/Project3/TodoApp/database.py
+++ my-course-code/Project3/TodoApp/database.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Ideally, a connection string as a variable.
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost/TodoApplicationDatabase'
 
 # Create an instance of a connection to the database at our database URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # a Session for connecting to and interacting with our database.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
