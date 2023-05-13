b9fb72d173ff79dbd5ff7aa0c8a19bd32e82b719
DJ Kemmet <kemmetdaniel@gmail.com>
2023-05-13 14:52:37 -0500

    Section 11 - Authenticate Requests - Assignments Complete

--- my-course-code/Project3/TodoApp/routers/users.py
+++ my-course-code/Project3/TodoApp/routers/users.py
@@ -2,11 +2,16 @@ from typing import Annotated
 #Framework Dependencies
 from fastapi import HTTPException, APIRouter, status, Depends
 # Application-specific Dependencies
-from .auth import get_current_user
 from database import SessionLocal
 from models import Users
+from .auth import get_current_user, SECRET_KEY, ALGORITHM, bcrypt_context
 #SQLAlchemy Dependencies
 from sqlalchemy.orm import Session
+# Pydantic Dependencies
+from pydantic import BaseModel, Field
+
+class PasswordChangeRequestModel(BaseModel):
+    new_password: str = Field(min_length=8)
 
 def get_db():
     db = SessionLocal()
@@ -17,6 +22,8 @@ def get_db():
 
 
 database_dependency = Annotated[Session, Depends(get_db)]
+
+
 user_dependency = Annotated[dict, Depends(get_current_user)]
 # Dependencies
 
@@ -34,5 +41,12 @@ async def get_users(user: user_dependency, db: database_dependency):
     return db.query(Users).filter(Users.id == user.get('id')).first()
 
 @router.post("/password-reset", status_code=status.HTTP_200_OK)
-async def reset_password(user: user_dependency, db: database_dependency):
-    pass
+async def reset_password(user: user_dependency, db: database_dependency, password: PasswordChangeRequestModel):
+    if user is None: 
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Login.")
+    new_password = bcrypt_context.hash(password.new_password)
+    current_user = db.query(Users).filter(Users.id == user.get('id')).first()
+    current_user.hashed_password = new_password
+    db.add(current_user)
+    db.commit()
+    return {"Message": "Password Updated",}
